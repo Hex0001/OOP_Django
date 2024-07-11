10fabcef5b56d0064de629b3d0bbebdee87f8986
Hex0001 <hex0001@mail.ru>
2024-07-11 19:49:07 +0500

    feat: Лабораторная 5 (практика)

--- app/views.py
+++ app/views.py
@@ -5,6 +5,52 @@ from django.shortcuts import redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import TemplateForm, CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views import View
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth.views import LoginView
+
+
+class MyLoginView(LoginView):
+    template_name = 'app/login.html'
+    redirect_authenticated_user = True
+
+
+class MyFormView(FormView):
+    template_name = 'app/template_form'
+    form_class = TemplateForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        return JsonResponse(form.cleaned_data)
+
+
+class MyTemplView(TemplateView):
+    template_name = 'app/template_form.html'
+
+    def post(self, request, *args, **kwargs):
+        received_data = request.POST  # Приняли данные в словарь
+        form = TemplateForm(received_data)
+        if form.is_valid():
+            return JsonResponse(data=form.cleaned_data,
+                                json_dumps_params={"indent": 4,
+                                                   "ensure_ascii": False})
+        context = self.get_context_data(**kwargs)
+        context["form"] = form
+        return self.render_to_response(context)
+
+
+class TemplView(View):
+    def get(self, request):
+        return render(request, 'app/template_form.html')
+
+    def post(self, request):
+        received_data = request.POST  # Приняли данные в словарь
+        form = TemplateForm(received_data)
+        if form.is_valid():
+            return JsonResponse(data=form.cleaned_data,
+                                json_dumps_params={"indent": 4,
+                                                   "ensure_ascii": False})
+        return render(request, 'app/template_form.html', context={'form': form})
 
 
 def template_view(request):
--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,15 @@ AUTHENTICATION_BACKENDS = (
    'django.contrib.auth.backends.ModelBackend',
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Пример настроек для использования консольного
+# бэкенда (для тестирования)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.ethereal.email'
+EMAIL_HOST_USER = 'bonnie.berge@ethereal.email'
+EMAIL_HOST_PASSWORD = 'GkjYvQ7BPnSPn51pvn'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+
 LOGIN_REDIRECT_URL = '/profile/'  # Чтобы после авторизации перенаправлялось на страницу пользователя
 
 SOCIAL_AUTH_GITHUB_KEY = 'Ov23liuq1ZkmnuWh6ddB'
